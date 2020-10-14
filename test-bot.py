92e86af2c197b64b094f1cfdd024a9a67317ed6d
Josh Tan <prof.whale@gmail.com>
2020-10-14 20:48:10 +0200

    started on basic timer

--- test-bot.py
+++ test-bot.py
@@ -1,16 +1,44 @@
 import discord
+import asyncio
 from token_info import my_token
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
 
-@client.event
-async def on_message(message):
-    message.content = message.content.lower()
+@bot.event
+async def on_ready():
+    print('Bot online..')
 
-    if message.author == client.user:
-        return
-    if message.content.startswith("hello"):
-        await message.channel.send("What up! bot is ready to mingle. Woop")
 
-client.run(my_token)
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+
+@bot.command()
+async def pom(ctx, seconds):
+    try:
+        second = int(seconds) * 60
+        if second <= 0:
+            await ctx.send('Negative number not accepted!')
+            print("neg num")
+            raise BaseException
+
+        message = await ctx.send(f'Timer: {second}')
+
+        while True:
+            second -= 1
+            if second == 0:
+                await message.edit(content='Ended!')
+                break
+
+            await message.edit(content=f'Timer: {second}')
+            await asyncio.sleep(1)
+
+        await ctx.send(f'{ctx.author.mention}, Brrr countdown ended!')
+    except ValueError:
+        await ctx.send('You must enter a number!')
+
+
+bot.run(my_token)
