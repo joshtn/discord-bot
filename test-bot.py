03876a2745c31a982e435f442e1c3dac883e4020
Josh Tan <prof.whale@gmail.com>
2020-11-06 23:31:16 +0100

    base done

--- test-bot.py
+++ test-bot.py
@@ -1,5 +1,5 @@
 import discord
-import asyncio
+import time
 from token_info import my_token
 from discord.ext import commands
 
@@ -11,32 +11,45 @@ async def on_ready():
     print('Bot online..')
 
 
+@bot.event
+async def on_raw_reaction_add(payload):
+    print(f"hello {payload.emoji.name}")
+
+
+@bot.event
+async def on_reaction_remove(reaction, user):
+    print(f"{user.display_name} removed reaction {reaction.emoji.name}")
+
+
 @bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 
 
 @bot.command()
-async def pom(ctx, seconds):
+async def pom(ctx, minutes):
     try:
-        second = int(seconds) * 60
-        if second <= 0:
+        t = int(minutes) * 60
+        if t <= 0:
             await ctx.send('Negative number not accepted!')
             print("neg num")
             raise BaseException
 
-        message = await ctx.send(f'Timer: {second}')
+        message = await ctx.send(f'Timer: {t}')
+        await message.add_reaction('❌')
 
         while True:
-            second -= 1
-            if second == 0:
-                await message.edit(content='Ended!')
+            mins = t // 60
+            secs = t % 60
+            if t == 0:
+                await message.edit(content='🍅 COMPLETE!')
                 break
 
-            await message.edit(content=f'Timer: {second}')
-            await asyncio.sleep(1)
+            await message.edit(content='🍅 {:02d}:{:02d}'.format(mins, secs))
+            time.sleep(1)
+            t -= 1
 
-        await ctx.send(f'{ctx.author.mention}, Brrr countdown ended!')
+        await ctx.send(f'{ctx.author.mention}, BRR countdown finished!')
     except ValueError:
         await ctx.send('You must enter a number!')
 
